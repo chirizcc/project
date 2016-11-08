460a64d01541f217ae78f2d7e11edcd8de7f1089
chirizcc <chirizcc@gmail.com>
2016-11-08 10:19:49 +0800

    完善404页面

--- Application/Admin/Controller/EmptyController.class.php
+++ Application/Admin/Controller/EmptyController.class.php
@@ -14,6 +14,7 @@ class EmptyController extends Controller
 {
     public function _empty()
     {
+        header('HTTP/1.0 404 not fount');
         $this->display('Public/notFount');
     }
 }
--- Application/Home/Controller/HomeController.class.php
+++ Application/Home/Controller/HomeController.class.php
@@ -14,6 +14,7 @@ class HomeController extends Controller
 {
     public function _empty()
     {
+        header('HTTP/1.0 404 not fount');
         $this->display('Public/notFount');
     }
 }
