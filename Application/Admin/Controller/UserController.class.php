ff2526c91568f82729ff03ccdef7d0bbf729cf54
chirizcc <chirizcc@gmail.com>
2016-11-10 21:32:56 +0800

    用户管理首页及完善后台头像

--- Application/Admin/Controller/UserController.class.php
+++ Application/Admin/Controller/UserController.class.php
@@ -4,9 +4,33 @@ namespace Admin\Controller;
 
 class UserController extends AdminController
 {
-    public function index()
+    public function index($search = null)
     {
+        $map = [];
+        if(!empty($search)) {
+            $map['u.u_username'] =  ['like','%'.$search.'%'];
+        }
+
+        $data = M('user')->table('zd_user as u,zd_detail as d')->where($map)->where('u.u_id = d.det_uid')->field('u.u_id id,u.u_username username,u.u_regtime time,u.u_istype type,d.det_name name,d.det_sex sex')->order('u.u_id desc')->page($_GET['p'],6)->select();
+
+        $this->assign('data',$data);
+
+        $count = M('user')->table('zd_user as u,zd_detail as d')->where($map)->where('u.u_id = d.det_uid')->count();// 查询满足要求的总记录数
+        // 使用自定义分页类
+        //$Page = new \Think\Page($count,1);
+        $Page = new \Org\Util\MyPage($count,6);// 实例化分页类 传入总记录数和每页显示的记录数
+
+        if(!empty($search)) {
+            //分页跳转的时候保证查询条件
+            foreach($map as $key=>$val) {
+                $Page->parameter[$key] = urlencode($val);
+            }
+        }
+
+        $show = $Page->show();// 分页显示输出
+        $this->assign('page',$show);// 赋值分页输出
         
+        $this->display();
     }
     
     public function myInfo()
@@ -22,9 +46,7 @@ class UserController extends AdminController
     {
         $user = M('detail');
         $data = I('post.');
-        if(false === $user->where('')->save($data)) {
-            echo $user->getLastSql();
-            die;
+        if(false === $user->save($data)) {
             $this->ajaxReturn(false);
         } else {
             $this->ajaxReturn(true);
