a02dfaac2e0958f6df5e9b190aa05da6962d8b43
syq <pgz28627183@164.com>
2016-11-11 14:58:39 +0800

    1111 1500

--- Application/Home/Controller/LoginController.class.php
+++ Application/Home/Controller/LoginController.class.php
@@ -13,7 +13,7 @@ class LoginController extends HomeController
 	    $username = trim($username);
 	    $password = $_POST['password'];
 	    $password = trim($password);
-	    // $password = md5($password);
+	    $password = md5($password);
 	    // var_dump($password);exit;
 
 	    $map = [];
@@ -23,6 +23,9 @@ class LoginController extends HomeController
    		//执行添加
 
         if (!empty($user)) {
+        	if($user['u_istype'] == '4'){
+        		$this->error('您的账号被禁用 请联系管理员');
+        	}
             session('name',$username);
             session('id',$user['u_id']);
             $this->success('恭喜您,登录成功!', U('Index/index'));
@@ -31,6 +34,15 @@ class LoginController extends HomeController
            $this->error('账号或密码错误....');
     		// $this->redirect('Login/index', array('tip' => '账号或密码错误'));
         }
+	}
+
+	public function logout(){
+		session(null);
+        $this->redirect('Index/index');
+	}
+
+	public function reg(){
+		
 	}
 
 }
