94e4705c55e9099f094be2bb0d2aee037e39b4b1
chirizcc <chirizcc@gmail.com>
2016-11-16 11:42:11 +0800

    完善书籍添加书籍修改

---
+++ Application/Admin/Model/BookModel.class.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Admin\Model;
+
+use Think\Model;
+
+// book表模型类
+class BookModel extends Model
+{
+    protected $_validate = array(
+        array('b_name','require','书籍名不能为空！',0,'regex',3), // 验证书籍名不能为空
+        array('b_tid','require','请选择分类！',0,'regex',3), // 验证分类不能为空
+        array('b_tid',[0],'请选择分类！',0,'notin',3), // 验证分类不能为空
+    );
+
+    protected $_auto = array (
+        array('b_time','time',2,'function'), // 对update_time字段在更新的时候写入当前时间戳
+    );
+}
