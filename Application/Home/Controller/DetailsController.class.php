cc3182a4b8fdb995bd279dfa7854a30b8923fdef
chirizcc <chirizcc@gmail.com>
2016-11-17 14:56:47 +0800

    书籍详情2.0

--- Application/Home/Controller/DetailsController.class.php
+++ Application/Home/Controller/DetailsController.class.php
@@ -9,11 +9,30 @@ class DetailsController extends Controller
             $this->redirect('Home/Index/index');
         }
         
-        $data = M('book')->find($b_id);
+        $data = M('book')->where(['b_status' => 1,'b_id' => $b_id])->field('b_id,b_name,b_tid,b_img,b_click,b_introduce,b_author')->find();
         if(empty($data)) {
-            $this->redirect('Home/Index/index');
+            $this->error('该书已下架');
         }
+
+        // 获取该书的分类
+        $res = M('type')->field('t_name,t_pid')->find($data['b_tid']);
+        $data['t_name'] = $res['t_name'];
+        $res = M('type')->field('t_name')->find($res['t_pid']);
+        $data['t_pname'] = $res['t_name'];
+
+        // 获取该书的目录
+        $catalog = M('catalog')->where(['cata_bid' => $b_id,'cata_status' => 1])->field('cata_id,cata_name')->order('cata_order')->select();
+        
+        $cataCount = M('catalog')->where(['cata_bid' => $b_id,'cata_status' => 1])->count();
+        $data['cataCount'] = $cataCount;
+
+        // 获取最后更新时间
+        $lastTime = M('catalog')->table('zd_catalog ca,zd_content co')->where('ca.cata_id = co.con_catid and ca.cata_status = 1 and ca.cata_bid = '.$b_id)->max('co.con_time');
+        $data['lastTime'] = empty($lastTime) ? '无' :date('Y-m-d H:i',$lastTime);
+
+        $this->assign('catalog',$catalog);
         $this->assign('data',$data);
         $this->display();
     }
+
 }
