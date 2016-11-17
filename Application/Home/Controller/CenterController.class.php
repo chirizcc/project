6c8789ce99ffae419163787b81ffe18391d02440
chirizcc <chirizcc@gmail.com>
2016-11-17 22:43:40 +0800

    完成前台个人信息查看及修改

--- Application/Home/Controller/CenterController.class.php
+++ Application/Home/Controller/CenterController.class.php
@@ -7,8 +7,8 @@ class CenterController extends JudgeController
 {
     public function index()
     {
-        $n_id = session('home_id');
-        $img = M('detail')->where(['det_uid' => $n_id])->field('det_img')->find();
+        $u_id = session('home_id');
+        $img = M('detail')->where(['det_uid' => $u_id])->field('det_img')->find();
         $this->assign('img',$img['det_img']);
         $this->display();
     }
@@ -16,5 +16,39 @@ class CenterController extends JudgeController
     public function show()
     {
         $this->display();
+    }
+
+    public function info()
+    {
+        $u_id = session('home_id');
+        $data = M('detail')->where(['det_uid' => $u_id])->find();
+
+        $res = M('user')->field('u_regtime')->find($u_id);
+        $data['regTime'] = date('Y-m-d H:i',$res['u_regtime']);
+
+        $this->assign('data',$data);
+        $this->display();
+    }
+    
+    public function edit()
+    {
+        $u_id = session('home_id');
+        $data = M('detail')->where(['det_uid' => $u_id])->find();
+        $this->assign('data',$data);
+        $this->display();
+    }
+    
+    public function update()
+    {
+        $de = D('detail');
+        if($de->create()) {
+            if(false === $de->save()) {
+                $this->error('编辑失败，请稍后再试!');
+            } else {
+                $this->success('编辑成功',U('Home/Center/info'));
+            }
+        } else {
+            $this->error($de->getError());
+        }
     }
 }
---
+++ Application/Home/Model/DetailModel.class.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Home\Model;
+
+use Think\Model;
+
+class DetailModel extends Model
+{
+    protected $_validate = array(
+        array('det_tel', '/^1[3|4|5|7|8][0-9]\d{8}$/', '手机号码格式不正确！'), // 正则验证用户名
+        array('det_email', 'email', '邮箱格式不正确'),
+    );
+}
