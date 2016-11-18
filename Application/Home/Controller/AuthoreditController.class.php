2925a8cd1150bd0ee9412d10a4823ecf87b26ead
Administrator <732528103@qq.com>
2016-11-18 16:44:33 +0800

    作者编辑啥也没做

---
+++ Application/Home/Controller/AuthoreditController.class.php
@@ -0,0 +1,12 @@
+<?php  
+	namespace Home\Controller;
+	use Think\Controller;
+
+	// 作者编辑
+	class AuthoreditController extends JudgeController
+	{
+		public function index()
+		{
+			$this->display();
+		}
+	}
