6d5c4029dd47935e3c03d400b6169acd934872b1
chirizcc <chirizcc@gmail.com>
2016-11-18 10:45:53 +0800

    前台修改头像完成

--- Application/Home/Controller/CenterController.class.php
+++ Application/Home/Controller/CenterController.class.php
@@ -51,4 +51,82 @@ class CenterController extends JudgeController
             $this->error($de->getError());
         }
     }
+
+    public function uploadPortrait()
+    {
+        $upload = new \Think\Upload();// 实例化上传类
+        $upload->maxSize = 3145728 ;// 设置附件上传大小
+        $upload->exts = array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+        $upload->rootPath = './Uploads/'; // 设置附件上传根目录
+        $upload->savePath = 'Portrait'; // 设置附件上传（子）目录
+// 上传文件
+        $info = $upload->upload();
+
+        if(!$info) {// 上传错误提示错误信息
+            $data = [
+                'code' => 1,
+                'msg' => $upload->getError(),
+            ];
+        }else{
+            $path = './Uploads/'.$info['file']['savepath'];
+            $name = $info['file']['savename'];
+
+            $image = new \Think\Image();
+            $image->open($path.$name);
+            // 按照原图的比例生成一个最大为150*150的缩略图并保存为thumb.jpg
+            $image->thumb(200, 200)->save($path.'s_'.$name);
+
+            //删除原图片
+            unlink($path.$name);
+
+            $data = [
+                'code' => 0,
+                'msg' => '上传成功',
+                'url' => $info['file']['savepath'] . 's_' . $name,
+            ];
+        }
+
+        $this->ajaxReturn($data);
+    }
+
+    public function insertPortrait()
+    {
+        $data = [
+            'code' => 1,
+            'msg' => '',
+        ];
+        $url = I('post.url');
+        if(is_file('./Uploads/'.$url)){
+            $u_id = session('home_id');
+            $res = M('detail')->where(['det_uid' => $u_id])->field('det_id,det_img')->find();
+            if(M('detail')->create(['det_id' => $res['det_id'],'det_img' => $url])) {
+                if(M('detail')->save()) {
+                    $data['code'] = 0;
+                    $data['msg'] = '头像保存成功';
+                    unlink('./Uploads/'.$res['det_img']);
+                } else {
+                    unlink('./Uploads/'.$url);
+                    $data['msg'] = '头像保存失败，请稍后再试!';
+                }
+            } else {
+                unlink('./Uploads/'.$url);
+                $data['msg'] = M('detail')->getError();
+            }
+        } else {
+            $data['msg'] = '头像保存失败，请稍后再试!';
+        }
+        $this->ajaxReturn($data);
+    }
+
+    // 点击重置按钮时删除图片
+    public function delImg()
+    {
+        $url = I('post.url');
+        if(empty($url)) {
+            return;
+        }
+
+        $path = './Uploads/'.$url;
+        unlink($path);
+    }
 }
