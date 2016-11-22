45fa5b6a3e02b8fa9a9976bc794eed3f69d8a9d5
chirizcc <chirizcc@gmail.com>
2016-11-22 20:11:54 +0800

    完善前台作者模块

--- Application/Admin/Controller/ContentController.class.php
+++ Application/Admin/Controller/ContentController.class.php
@@ -6,7 +6,7 @@ class ContentController extends AdminController
 {
     public function index($cata_id = null)
     {
-        if(empty($cata_id)) {
+        if (empty($cata_id)) {
             $this->redirect('Admin/Book/index');
             die;
         }
@@ -15,15 +15,15 @@ class ContentController extends AdminController
 
         $cata_name = M('catalog')->field('cata_name')->find($cata_id);
 
-        $this->assign('cata_id',$cata_id);
-        $this->assign('cata_name',$cata_name['cata_name']);
-        $this->assign('data',$content);
+        $this->assign('cata_id', $cata_id);
+        $this->assign('cata_name', $cata_name['cata_name']);
+        $this->assign('data', $content);
         $this->display();
     }
 
     public function edit($cata_id = null)
     {
-        if(empty($cata_id)) {
+        if (empty($cata_id)) {
             $this->redirect('Admin/Book/index');
             die;
         }
@@ -32,22 +32,22 @@ class ContentController extends AdminController
 
         $cata_name = M('catalog')->field('cata_name')->find($cata_id);
 
-        $this->assign('cata_id',$cata_id);
-        $this->assign('cata_name',$cata_name['cata_name']);
-        $this->assign('data',$content);
+        $this->assign('cata_id', $cata_id);
+        $this->assign('cata_name', $cata_name['cata_name']);
+        $this->assign('data', $content);
         $this->display();
     }
-    
+
     public function update($cata_id = null)
     {
         $con = D('content');
-        if($con->create($_POST)) {
-            if($con->save()) {
+        if ($con->create($_POST)) {
+            if ($con->save()) {
                 // 编辑过内容，这将该目录状态更改为未审核
                 $data['cata_status'] = 0;
                 $data['cata_id'] = $cata_id;
                 M('catalog')->save($data);
-                $this->success('修改成功',U('Admin/Content/index',['cata_id' => $cata_id]));
+                $this->success('修改成功', U('Admin/Content/index', ['cata_id' => $cata_id]));
             } else {
                 $this->error('修改失败');
             }
---
+++ Application/Home/Model/ContentModel.class.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Home\Model;
+
+use Think\Model;
+
+class ContentModel extends Model
+{
+    protected $_validate = array(
+        array('con_catid','require','章节不能为空！',0,'regex',3), // 验证书籍名不能为空
+    );
+
+    protected $_auto = array (
+        array('con_time','time',3,'function'), // 对con_time字段在任何时候都进行更新
+        array('con_content','',1,'string'), // 对con_content字段在添加新章节是为空
+    );
+}
