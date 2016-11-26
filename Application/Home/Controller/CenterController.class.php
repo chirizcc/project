e7f699ab253d576a72d7b23d027b2fb08536da02
chirizcc <chirizcc@gmail.com>
2016-11-26 14:24:14 +0800

    修复个人中心头像上传小bug

--- Application/Home/Controller/CenterController.class.php
+++ Application/Home/Controller/CenterController.class.php
@@ -78,11 +78,12 @@ class CenterController extends JudgeController
     public function uploadPortrait()
     {
         $upload = new \Think\Upload();// 实例化上传类
-        $upload->maxSize = 3145728;// 设置附件上传大小
+        //$upload->maxSize = 3145728;// 设置附件上传大小
+        $upload->maxSize = 2097152;// 设置附件上传大小
         $upload->exts = array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
         $upload->rootPath = './Uploads/'; // 设置附件上传根目录
         $upload->savePath = 'Portrait'; // 设置附件上传（子）目录
-// 上传文件
+        // 上传文件
         $info = $upload->upload();
 
         if (!$info) {// 上传错误提示错误信息
